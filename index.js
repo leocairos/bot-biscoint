dbbb488385b916534face390c547c773aa3a039b
leocairos <leocairos@gmail.com>
2022-03-08 21:07:32 -0300

    send variation by time to telegram chat

--- index.js
+++ index.js
@@ -8,6 +8,49 @@ const COIN_PAIRS = process.env.COIN_PAIRS.split(';');
 const PERCENT_ALERT = Number(process.env.PERCENT_ALERT);
 // Ticker rate limit is 12 per minute => 1 each 5s
 const rateInterval = COIN_PAIRS.length * 5100;
+const averageOf = Number(process.env.INTERVAL_ALERT) / 5100;
+const prices = [];
+
+function coinCalc() {
+    const coins = [];
+    prices.forEach( coin => {
+        const index = coins.findIndex( c => c.coinPair === coin.coinPair);
+        if (index >= 0){
+            if (coin.percentage > coins[index].max) coins[index].max = coin.percentage;
+            if (coin.percentage < coins[index].min) coins[index].min = coin.percentage;
+            coins[index].avg = (coins[index].avg + coin.percentage) / 2;
+        } else {
+            const coinPair = coin.coinPair;
+            const max = coin.percentage;
+            const min = coin.percentage;
+            const avg = coin.percentage;
+            coins.push({coinPair, max, min, avg});
+        }
+    })
+    return coins;
+}
+
+function formatMsgCalc(calc){
+    let msg = `Result of the last ${Number(process.env.INTERVAL_ALERT)/1000/60} `
+    msg += `minutes of price change monitoring:\n`;
+    calc.forEach( c => {
+        msg += `  -> ${c.coinPair} avg: ${(c.avg*100).toFixed(2)}% `
+        msg += `min: ${(c.min*100).toFixed(2)} `
+        msg += `max: ${(c.max*100).toFixed(2)}\n`
+    })
+    return msg;
+}
+
+// Average, Max and Min prices by coinPair
+function processPrices(coinPair, percentage) {
+    prices.push({coinPair, percentage});
+    if (prices.length >= averageOf) {
+        const calc = coinCalc();
+        console.log(formatMsgCalc(calc));
+        telegram.sendMessage(formatMsgCalc(calc));
+        prices.length = 0;
+    }
+}
 
 async function app() {
 
@@ -20,6 +63,7 @@ async function app() {
         const msg = `${base}/${quote} [${(percentage*100).toFixed(2)}%] ask: ${ask} bid: ${bid}`
         console.log(msg);
         if (percentage >= PERCENT_ALERT) telegram.sendMessage(msg);
+        processPrices(coinPair, percentage);
     })
 }
 
